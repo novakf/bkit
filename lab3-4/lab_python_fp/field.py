da44fc8c3557d273bd648db3ea5ebfa570c040de
novakf <bogdannovikov116@mail.ru>
2022-11-08 23:13:32 +0300

    lab

--- lab3-4/lab_python_fp/field.py
+++ lab3-4/lab_python_fp/field.py
@@ -11,7 +11,7 @@ def field(items, *args):
         else: 
             yield d
 '''
-goods = [
+
    {'title': 'Ковер', 'price': 2000, 'color': 'green'},
    {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'}
 ]
